e957685237e10b7c09f8be36a3f138a7daa4da8f
Damian Szwed <damian.szwed@10g.pl>
2018-03-17 21:53:16 +0100

    POST rest stub for flashcard

--- src/main/java/com/github/damianszwed/fishky/proxy/config/ApplicationConfiguration.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/config/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.github.damianszwed.fishky.proxy.config;
 import com.github.damianszwed.fishky.proxy.adapter.FlashcardStorage;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardProvider;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardRemover;
+import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardSaver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,11 @@ public class ApplicationConfiguration {
 
     @Bean
     FlashcardRemover flashcardRemover(FlashcardStorage flashcardStorage) {
+        return flashcardStorage;
+    }
+
+    @Bean
+    FlashcardSaver flashcardSaver(FlashcardStorage flashcardStorage) {
         return flashcardStorage;
     }
 }
--- src/main/java/com/github/damianszwed/fishky/proxy/flashcard/FlashcardController.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/flashcard/FlashcardController.java
@@ -3,10 +3,12 @@ package com.github.damianszwed.fishky.proxy.flashcard;
 import com.github.damianszwed.fishky.proxy.port.flashcard.Flashcard;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardProvider;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardRemover;
+import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardSaver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,11 +22,13 @@ public class FlashcardController {
 
     private FlashcardProvider flashcardProvider;
     private FlashcardRemover flashcardRemover;
+    private FlashcardSaver flashcardSaver;
 
     @Autowired
-    public FlashcardController(FlashcardProvider flashcardProvider, FlashcardRemover flashcardRemover) {
+    public FlashcardController(FlashcardProvider flashcardProvider, FlashcardRemover flashcardRemover, FlashcardSaver flashcardSaver) {
         this.flashcardProvider = flashcardProvider;
         this.flashcardRemover = flashcardRemover;
+        this.flashcardSaver = flashcardSaver;
     }
 
     @RequestMapping("/flashcards")
@@ -32,10 +36,17 @@ public class FlashcardController {
         return flashcardProvider.getFlashcards("anyUsername");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "flashcard/{flashcardId:.+}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/flashcard/{flashcardId:.+}")
     @ResponseStatus(value = HttpStatus.OK)
     public void removeFlashcard(@PathVariable String flashcardId) {
         flashcardRemover.removeFlashcard(flashcardId);
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/flashcard")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void saveFlashcard(@RequestBody Flashcard flashcard) {
+        flashcardSaver.saveFlashcard(flashcard);
+    }
+
 
 }
