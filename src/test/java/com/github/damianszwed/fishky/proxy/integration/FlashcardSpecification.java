7cda746997e6849317e2b3461003ed7ba8521cb5
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2018-03-17 21:23:33 +0100

    Removing flashcard case implemented

--- src/test/java/com/github/damianszwed/fishky/proxy/integration/FlashcardSpecification.java
+++ src/test/java/com/github/damianszwed/fishky/proxy/integration/FlashcardSpecification.java
@@ -1,4 +1,4 @@
-package com.github.damianszwed.fishky.proxy.component;
+package com.github.damianszwed.fishky.proxy.integration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,17 +23,15 @@ public class FlashcardSpecification {
     private MockMvc mockMvc;
 
     @Test
-    public void getAllFlashcardsShouldReturnMockedFlashcard() throws Exception {
+    public void removingOneFlashcard() throws Exception {
+        this.mockMvc.perform(delete("/flashcard/user1@example.com-questionb")).andExpect(status().isOk());
+
         this.mockMvc.perform(get("/flashcards")).andDo(print()).andExpect(status().isOk())
             .andExpect(jsonPath("$[0].id").value("user1@example.com-questiona"))
             .andExpect(jsonPath("$[0].question").value("questionA"))
             .andExpect(jsonPath("$[0].answer").value("answerA"))
-            .andExpect(jsonPath("$[1].id").value("user1@example.com-questionb"))
-            .andExpect(jsonPath("$[1].question").value("questionB"))
-            .andExpect(jsonPath("$[1].answer").value("answerB"))
-            .andExpect(jsonPath("$[2].id").value("user1@example.com-questionc"))
-            .andExpect(jsonPath("$[2].question").value("questionC"))
-            .andExpect(jsonPath("$[2].answer").value("answerC"))
-        ;
+            .andExpect(jsonPath("$[1].id").value("user1@example.com-questionc"))
+            .andExpect(jsonPath("$[1].question").value("questionC"))
+            .andExpect(jsonPath("$[1].answer").value("answerC"));
     }
 }
