fc71a71e0db5cc6a2a99bb3356bb5103f7b6321b
Damian Szwed <damian.szwed@10g.pl>
2018-03-17 20:29:44 +0100

    Introduced spotbugs and checkstyle

---
+++ src/main/java/com/github/damianszwed/fishky/proxy/port/flashcard/FlashcardProvider.java
@@ -0,0 +1,7 @@
+package com.github.damianszwed.fishky.proxy.port.flashcard;
+
+import java.util.List;
+
+public interface FlashcardProvider {
+    List<Flashcard> getFlashcards(String username);
+}
---
+++ src/main/java/com/github/damianszwed/fishky/proxy/port/flashcard/FlashcardUpdater.java
@@ -0,0 +1,5 @@
+package com.github.damianszwed.fishky.proxy.port.flashcard;
+
+public interface FlashcardUpdater {
+    void updateFlashcard(Flashcard flashcard);
+}
