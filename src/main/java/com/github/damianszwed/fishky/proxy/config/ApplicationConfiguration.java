7657634bd6f4c4c0aec8050760ad7c6a48e3cbd4
Damian Szwed <damian.szwed@10g.pl>
2018-03-17 21:23:33 +0100

    Removing flashcard case implemented

--- src/main/java/com/github/damianszwed/fishky/proxy/config/ApplicationConfiguration.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/config/ApplicationConfiguration.java
@@ -2,6 +2,7 @@ package com.github.damianszwed.fishky.proxy.config;
 
 import com.github.damianszwed.fishky.proxy.adapter.FlashcardStorage;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardProvider;
+import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardRemover;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +12,10 @@ public class ApplicationConfiguration {
     @Bean
     FlashcardProvider flashcardProvider() {
         return new FlashcardStorage();
+    }
+
+    @Bean
+    FlashcardRemover flashcardRemover(FlashcardStorage flashcardStorage) {
+        return flashcardStorage;
     }
 }
