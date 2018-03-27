b428c17359333972a85a7494879df14a255b3f22
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2018-03-27 21:24:19 +0200

    Flashcard dvelopment storage

--- src/main/java/com/github/damianszwed/fishky/proxy/adapter/FlashcardStorage.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/adapter/FlashcardStorage.java
@@ -1,50 +1,35 @@
 package com.github.damianszwed.fishky.proxy.adapter;
 
+import com.github.damianszwed.fishky.proxy.adapter.storage.FlashcardRepository;
 import com.github.damianszwed.fishky.proxy.port.flashcard.Flashcard;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardProvider;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardRemover;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardSaver;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.Arrays.asList;
 
 @Slf4j
 @Scope("singleton")
 public class FlashcardStorage implements FlashcardProvider, FlashcardRemover, FlashcardSaver {
 
-    private List<Flashcard> flashcards = new ArrayList<>();
-
-    public FlashcardStorage() {
-        flashcards.addAll(
-            asList(
-                Flashcard.builder().id("user1@example.com-questiona").question("questionA").answer("answerA").build(),
-                Flashcard.builder().id("user1@example.com-questionb").question("questionB").answer("answerB").build(),
-                Flashcard.builder().id("user1@example.com-questionc").question("questionC").answer("answerC").build()
-            )
-        );
-    }
+    @Autowired
+    FlashcardRepository repository;
 
     @Override
     public List<Flashcard> getFlashcards(String username) {
-        return flashcards;
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public void removeFlashcard(String id) {
-        flashcards.removeIf(givenFlashcard -> givenFlashcard.getId().equals(id));
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public void saveFlashcard(Flashcard flashcard) {
-        removeFlashcard("user1@example.com-" + flashcard.getQuestion().toLowerCase());
-        flashcards.add(Flashcard.builder()
-            .answer(flashcard.getAnswer())
-            .question(flashcard.getQuestion())
-            .id("user1@example.com-" + flashcard.getQuestion().toLowerCase())
-            .build());
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
