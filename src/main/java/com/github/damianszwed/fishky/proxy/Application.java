d00bf47e1f72f191d9cede7d0cc79dd7120e1e92
Damian Szwed <damian.szwed@10g.pl>
2018-03-30 22:38:54 +0200

    add health check

--- src/main/java/com/github/damianszwed/fishky/proxy/Application.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/Application.java
@@ -2,9 +2,30 @@ package com.github.damianszwed.fishky.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    /**
+     * (Optional) App Engine health check endpoint mapping.
+     * @see <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking"></a>
+     * If your app does not handle health checks, a HTTP 404 response is interpreted
+     *     as a successful reply.
+     */
+    @RequestMapping("/_ah/health")
+    public String healthy() {
+        // Message body required though ignored
+        return "Still surviving.";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
