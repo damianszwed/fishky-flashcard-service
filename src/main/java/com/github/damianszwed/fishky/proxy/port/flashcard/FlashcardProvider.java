73d0b7e37f4fa58ad2280e343b24a6e0d697db16
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2018-12-04 10:53:36 +0100

    New gralde version, project reset

--- src/main/java/com/github/damianszwed/fishky/proxy/port/flashcard/FlashcardProvider.java
+++
@@ -1,7 +0,0 @@
-package com.github.damianszwed.fishky.proxy.port.flashcard;
-
-import java.util.List;
-
-public interface FlashcardProvider {
-    List<Flashcard> getFlashcards(String username);
-}
--- src/main/java/com/github/damianszwed/fishky/proxy/port/flashcard/FlashcardRemover.java
+++
@@ -1,5 +0,0 @@
-package com.github.damianszwed.fishky.proxy.port.flashcard;
-
-public interface FlashcardRemover {
-    void removeFlashcard(String id);
-}
