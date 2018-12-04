5e4621f9446d882d52328fe875d6b20ed714b342
Damian Szwed <damian.szwed@10g.pl>
2018-12-04 10:53:36 +0100

    New gralde version, project reset

--- src/main/java/com/github/damianszwed/fishky/proxy/adapter/storage/production/FlashcardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.damianszwed.fishky.proxy.adapter.storage.production;
-
-import com.github.damianszwed.fishky.proxy.port.flashcard.Flashcard;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FlashcardRepository extends MongoRepository<Flashcard, String> {
-}
