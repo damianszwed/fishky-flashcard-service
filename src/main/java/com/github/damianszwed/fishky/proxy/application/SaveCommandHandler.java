37fb5d12bde6ad412dec096eb8038a6e9edfd1db
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2019-01-11 21:59:12 +0100

    Saving flashcard is done (MVP)

---
+++ src/main/java/com/github/damianszwed/fishky/proxy/application/SaveCommandHandler.java
@@ -0,0 +1,26 @@
+package com.github.damianszwed.fishky.proxy.application;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.accepted;
+
+import com.github.damianszwed.fishky.proxy.port.CommandQueryHandler;
+import com.github.damianszwed.fishky.proxy.port.flashcard.Flashcard;
+import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardSaver;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public class SaveCommandHandler implements CommandQueryHandler {
+
+  private FlashcardSaver flashcardSaver;
+
+  public SaveCommandHandler(FlashcardSaver flashcardSaver) {
+    this.flashcardSaver = flashcardSaver;
+  }
+
+  @Override
+  public Mono<ServerResponse> handle(ServerRequest serverRequest) {
+    return serverRequest.bodyToMono(Flashcard.class)
+        .doOnNext(flashcard -> flashcardSaver.saveFlashcard(flashcard))
+        .flatMap(flashcard -> accepted().build());
+  }
+}
