19ff292a1fa30b7e338107ad1131e6f3bea937b7
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2019-01-16 16:42:46 +0100

    cors filter has been implemented

--- src/main/java/com/github/damianszwed/fishky/proxy/configuration/CommandQueryWebConfiguration.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/configuration/CommandQueryWebConfiguration.java
@@ -18,30 +18,51 @@ import com.github.damianszwed.fishky.proxy.port.flashcard.EventSource;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardProvider;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardRemover;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardSaver;
+import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
 
 @Configuration
 @EnableWebFlux
 public class CommandQueryWebConfiguration {
 
   @Bean
-  public WebFluxConfigurer corsConfigurer() {
-    return new WebFluxConfigurerComposite() {
-
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
-            .allowedMethods("*");
-      }
-    };
+  public WebFilter corsFilter() {
+    return (ServerWebExchange ctx, WebFilterChain chain) -> Optional.of(ctx.getRequest())
+        .filter(CorsUtils::isCorsRequest)
+        .map(request -> {
+          ServerHttpResponse response1 = ctx.getResponse();
+          HttpHeaders headers = response1.getHeaders();
+          headers.add("Access-Control-Allow-Origin", "*");
+          headers.add("Access-Control-Allow-Methods",
+              "GET, PUT, POST, DELETE, OPTIONS");
+          headers.add("Access-Control-Max-Age", "3600");
+          headers.add("Access-Control-Allow-Headers",
+              "x-requested-with, authorization, Content-Type, "
+                  + "Authorization, credential, X-XSRF-TOKEN");
+          return request;
+        })
+        .filter(serverHttpRequest -> serverHttpRequest.getMethod() == HttpMethod.OPTIONS)
+        .<Mono<Void>>map(serverHttpRequests -> {
+          ServerHttpResponse response = ctx.getResponse();
+          response.setStatusCode(HttpStatus.OK);
+          return Mono.empty();
+        })
+        .orElseGet(() -> chain.filter(ctx));
   }
 
   @Bean
