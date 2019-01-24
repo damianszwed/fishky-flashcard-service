fba9b67f8416e97d268938296d1d1271cbe3654d
Damian Szwed <damian.szwed@10g.pl>
2019-01-24 13:31:46 +0100

    Fixed flux bug with saving flashcard

--- src/main/java/com/github/damianszwed/fishky/proxy/adapter/storage/production/FlashcardProductionStorage.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/adapter/storage/production/FlashcardProductionStorage.java
@@ -4,8 +4,10 @@ import com.github.damianszwed.fishky.proxy.port.flashcard.Flashcard;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardProvider;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardRemover;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardSaver;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 public class FlashcardProductionStorage implements FlashcardProvider, FlashcardRemover,
     FlashcardSaver {
 
@@ -27,6 +29,12 @@ public class FlashcardProductionStorage implements FlashcardProvider, FlashcardR
 
   @Override
   public void save(Flashcard flashcard) {
-    flashcardMongoRepository.save(flashcard);
+    flashcardMongoRepository.save(
+        flashcard
+            .toBuilder()
+            .id("user1@example.com-" + flashcard.getQuestion().toLowerCase())
+            .build())
+        .doOnError(throwable -> log.info(throwable.getMessage(), throwable))
+        .subscribe(newFlashcard -> log.info(newFlashcard.getId() + " has been created"));
   }
 }
