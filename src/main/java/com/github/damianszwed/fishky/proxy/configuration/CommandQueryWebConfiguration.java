e65b6c557a24b3b6c7f988033c8191a1ad7d95d6
Damian Szwed <damian.szwed@10g.pl>
2020-05-01 22:39:55 +0200

    Implemented simple CRUD for flashcard groups.

--- src/main/java/com/github/damianszwed/fishky/proxy/configuration/CommandQueryWebConfiguration.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/configuration/CommandQueryWebConfiguration.java
@@ -9,12 +9,16 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 import com.github.damianszwed.fishky.proxy.business.FlashcardDeleteCommandHandler;
 import com.github.damianszwed.fishky.proxy.business.FlashcardGetAllCommandHandler;
 import com.github.damianszwed.fishky.proxy.business.FlashcardGetAllQueryHandler;
+import com.github.damianszwed.fishky.proxy.business.FlashcardGroupDeleteCommandHandler;
+import com.github.damianszwed.fishky.proxy.business.FlashcardGroupGetAllQueryHandler;
+import com.github.damianszwed.fishky.proxy.business.FlashcardGroupSaveCommandHandler;
 import com.github.damianszwed.fishky.proxy.business.FlashcardProviderFlow;
 import com.github.damianszwed.fishky.proxy.business.FlashcardSaveCommandHandler;
 import com.github.damianszwed.fishky.proxy.business.FlashcardServerSentEventHandler;
 import com.github.damianszwed.fishky.proxy.business.SwaggerHandler;
 import com.github.damianszwed.fishky.proxy.port.CommandQueryHandler;
 import com.github.damianszwed.fishky.proxy.port.flashcard.EventSource;
+import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardGroupStorage;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardStorage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,28 +31,47 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class CommandQueryWebConfiguration {
 
   @Bean
-  public CommandQueryHandler getAllQueryHandler(FlashcardStorage flashcardStorage) {
+  public CommandQueryHandler flashcardGetAllQueryHandler(FlashcardStorage flashcardStorage) {
     return new FlashcardGetAllQueryHandler(flashcardStorage);
   }
 
   @Bean
-  public CommandQueryHandler saveCommandHandler(FlashcardStorage flashcardStorage) {
+  public CommandQueryHandler flashcardSaveCommandHandler(FlashcardStorage flashcardStorage) {
     return new FlashcardSaveCommandHandler(flashcardStorage);
   }
 
   @Bean
-  public CommandQueryHandler deleteCommandHandler(FlashcardStorage flashcardStorage) {
+  public CommandQueryHandler flashcardDeleteCommandHandler(FlashcardStorage flashcardStorage) {
     return new FlashcardDeleteCommandHandler(flashcardStorage);
   }
 
   @Bean
-  public CommandQueryHandler getAllCommandHandler(FlashcardProviderFlow flashcardProviderFlow) {
+  public CommandQueryHandler flashcardGetAllCommandHandler(
+      FlashcardProviderFlow flashcardProviderFlow) {
     return new FlashcardGetAllCommandHandler(flashcardProviderFlow);
   }
 
   @Bean
-  public CommandQueryHandler eventHandler(EventSource eventSource) {
+  public CommandQueryHandler flashcardServerSentEventHandler(EventSource eventSource) {
     return new FlashcardServerSentEventHandler(eventSource);
+  }
+
+  @Bean
+  public CommandQueryHandler flashcardGroupGetAllQueryHandler(
+      FlashcardGroupStorage flashcardGroupStorage) {
+    return new FlashcardGroupGetAllQueryHandler(flashcardGroupStorage);
+  }
+
+  @Bean
+  public CommandQueryHandler flashcardGroupSaveCommandHandler(
+      FlashcardGroupStorage flashcardGroupStorage) {
+    return new FlashcardGroupSaveCommandHandler(flashcardGroupStorage);
+  }
+
+  @Bean
+  public CommandQueryHandler flashcardGroupDeleteCommandHandler(
+      FlashcardGroupStorage flashcardGroupStorage) {
+    return new FlashcardGroupDeleteCommandHandler(flashcardGroupStorage);
   }
 
   @Bean
@@ -58,20 +81,30 @@ public class CommandQueryWebConfiguration {
 
   @Bean
   public RouterFunction<ServerResponse> flashcardRoutes(
-      CommandQueryHandler getAllQueryHandler,
-      CommandQueryHandler saveCommandHandler,
-      CommandQueryHandler deleteCommandHandler) {
-    return route(GET("/flashcards"), getAllQueryHandler::handle)
-        .andRoute(POST("/flashcards"), saveCommandHandler::handle)
-        .andRoute(DELETE("/flashcards/{id}"), deleteCommandHandler::handle);
+      CommandQueryHandler flashcardGetAllQueryHandler,
+      CommandQueryHandler flashcardSaveCommandHandler,
+      CommandQueryHandler flashcardDeleteCommandHandler) {
+    return route(GET("/flashcards"), flashcardGetAllQueryHandler::handle)
+        .andRoute(POST("/flashcards"), flashcardSaveCommandHandler::handle)
+        .andRoute(DELETE("/flashcards/{id}"), flashcardDeleteCommandHandler::handle);
+  }
+
+  @Bean
+  public RouterFunction<ServerResponse> flashcardGroupRoutes(
+      CommandQueryHandler flashcardGroupGetAllQueryHandler,
+      CommandQueryHandler flashcardGroupSaveCommandHandler,
+      CommandQueryHandler flashcardGroupDeleteCommandHandler) {
+    return route(GET("/flashcardGroups"), flashcardGroupGetAllQueryHandler::handle)
+        .andRoute(POST("/flashcardGroups"), flashcardGroupSaveCommandHandler::handle)
+        .andRoute(DELETE("/flashcardGroups/{id}"), flashcardGroupDeleteCommandHandler::handle);
   }
 
   @Bean
   public RouterFunction<ServerResponse> sseRoutes(
-      CommandQueryHandler getAllCommandHandler,
-      CommandQueryHandler eventHandler) {
-    return route(GET("/getAllFlashcardsCommand"), getAllCommandHandler::handle)
-        .andRoute(GET("/flashcardsEventStream"), eventHandler::handle);
+      CommandQueryHandler flashcardGetAllCommandHandler,
+      CommandQueryHandler flashcardServerSentEventHandler) {
+    return route(GET("/getAllFlashcardsCommand"), flashcardGetAllCommandHandler::handle)
+        .andRoute(GET("/flashcardsEventStream"), flashcardServerSentEventHandler::handle);
   }
 
   @Bean
--- src/main/java/com/github/damianszwed/fishky/proxy/port/flashcard/Flashcard.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/port/flashcard/Flashcard.java
@@ -1,5 +1,6 @@
 package com.github.damianszwed.fishky.proxy.port.flashcard;
 
+import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Value;
 
@@ -8,6 +9,8 @@ import lombok.Value;
 public class Flashcard {
 
   String id;
+  @NotNull
   String question;
+  @NotNull
   String answer;
 }
