c98ada19b835a55048b9d4185ff7701d8885e3e2
Damian Szwed <damian.szwed@10g.pl>
2020-05-01 13:15:34 +0200

    Updated to Spring Boot 2.2.6

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 [![Build Status](https://travis-ci.org/damianszwed/fishky-proxy.svg?branch=master)](https://travis-ci.org/damianszwed/fishky-proxy)
 
 # fishky-proxy
+
+## code-formatter
+In the project there is used [Google Java Style Guide](https://google.github.io/styleguide/javaguide.html)
+
+For IntelliJ IDEA please import [that](https://github.com/google/styleguide/blob/gh-pages/intellij-java-google-style.xml) style.
--- src/main/java/com/github/damianszwed/fishky/proxy/Application.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/Application.java
@@ -1,22 +1,9 @@
 package com.github.damianszwed.fishky.proxy;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {
-    MongoDataAutoConfiguration.class,
-    MongoReactiveDataAutoConfiguration.class,
-    MongoReactiveRepositoriesAutoConfiguration.class,
-    MongoAutoConfiguration.class,
-    MongoReactiveAutoConfiguration.class
-})
 public class Application {
 
   public static void main(String[] args) {
