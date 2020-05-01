aa22f68412534e154e11ab6a446422a170c7474a
Damian Szwed <damian.szwed@10g.pl>
2020-05-01 23:44:31 +0200

    FlashcardGroupSaveCommandHandler

--- src/main/java/com/github/damianszwed/fishky/proxy/business/FlashcardGroupDeleteFlashcardCommandHandler.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/business/FlashcardGroupDeleteFlashcardCommandHandler.java
@@ -1,7 +1,13 @@
 package com.github.damianszwed.fishky.proxy.business;
 
+import static org.springframework.web.reactive.function.server.ServerResponse.accepted;
+
 import com.github.damianszwed.fishky.proxy.port.CommandQueryHandler;
+import com.github.damianszwed.fishky.proxy.port.flashcard.Flashcard;
+import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardGroup;
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardGroupStorage;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
@@ -16,7 +22,30 @@ public class FlashcardGroupDeleteFlashcardCommandHandler implements CommandQuery
 
   @Override
   public Mono<ServerResponse> handle(ServerRequest serverRequest) {
-    ///flashcardGroups/{flashcardGroupId}/flashcards/{flashcardId}
-    return null;
+    String flashcardGroupId = serverRequest.pathVariable("flashcardGroupId");
+    String flashcardId = serverRequest.pathVariable("flashcardId");
+    return Mono.fromSupplier(() -> Void.TYPE)
+        .doOnNext((v) -> removeFlashcardFromFlashcardGroup(flashcardGroupId, flashcardId))
+        .flatMap(p -> accepted().build());
+  }
+
+  private void removeFlashcardFromFlashcardGroup(
+      String flashcardGroupId,
+      String flashcardId) {
+    flashcardGroupStorage.getById(flashcardGroupId)
+        .subscribe(flashcardGroup ->
+            flashcardGroupStorage.save(
+                flashcardGroup.toBuilder()
+                    .flashcards(withRemovedParticularFlashcard(flashcardId, flashcardGroup)
+                        .collect(Collectors.toList()))
+                    .build()));
+  }
+
+  private Stream<Flashcard> withRemovedParticularFlashcard(
+      String flashcardId,
+      FlashcardGroup flashcardGroup) {
+    return flashcardGroup.getFlashcards().stream()
+        .filter(flashcard -> !flashcard.getId()
+            .equals(flashcardId));
   }
 }
