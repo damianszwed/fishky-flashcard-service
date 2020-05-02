cd59733c62b333de4e5f592a26a388a0971d9352
Damian Szwed <damian.szwed@10g.pl>
2020-05-02 13:27:39 +0200

    Removed FlashcardStorageAdapter.

--- src/main/java/com/github/damianszwed/fishky/proxy/business/FlashcardDeleteCommandHandler.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/business/FlashcardDeleteCommandHandler.java
@@ -3,23 +3,33 @@ package com.github.damianszwed.fishky.proxy.business;
 import static org.springframework.web.reactive.function.server.ServerResponse.accepted;
 
 import com.github.damianszwed.fishky.proxy.port.CommandQueryHandler;
-import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardStorage;
+import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardGroupStorage;
+import java.util.stream.Collectors;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 public class FlashcardDeleteCommandHandler implements CommandQueryHandler {
 
-  private FlashcardStorage flashcardStorage;
+  private FlashcardGroupStorage flashcardGroupStorage;
 
-  public FlashcardDeleteCommandHandler(FlashcardStorage flashcardStorage) {
-    this.flashcardStorage = flashcardStorage;
+  public FlashcardDeleteCommandHandler(FlashcardGroupStorage flashcardGroupStorage) {
+    this.flashcardGroupStorage = flashcardGroupStorage;
   }
 
   @Override
   public Mono<ServerResponse> handle(ServerRequest serverRequest) {
     return Mono.fromSupplier(() -> Void.TYPE)
-        .doOnNext((v) -> flashcardStorage.remove(serverRequest.pathVariable("id")))
+        .doOnNext((v) -> removeFlashcardFromDefaultGroup(serverRequest.pathVariable("id")))
         .flatMap(p -> accepted().build());
+  }
+
+  private void removeFlashcardFromDefaultGroup(String id) {
+    flashcardGroupStorage.get("any", "default").subscribe(flashcardGroup ->
+        flashcardGroupStorage.save(flashcardGroup.toBuilder()
+            .flashcards(flashcardGroup.getFlashcards().stream()
+                .filter(flashcard -> !flashcard.getId().equals(id))
+                .collect(Collectors.toList()))
+            .build()));
   }
 }
