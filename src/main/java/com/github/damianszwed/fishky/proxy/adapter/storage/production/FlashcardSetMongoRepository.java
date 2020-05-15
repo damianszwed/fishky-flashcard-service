ba46f3b7dd1fc2dce2de9bb2846f3ab710ca9d34
Damian Szwed <damian.szwed@10g.pl>
2020-05-15 21:39:18 +0200

    Authorisation part 3.

--- src/main/java/com/github/damianszwed/fishky/proxy/adapter/storage/production/FlashcardSetMongoRepository.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/adapter/storage/production/FlashcardSetMongoRepository.java
@@ -2,10 +2,13 @@ package com.github.damianszwed.fishky.proxy.adapter.storage.production;
 
 import com.github.damianszwed.fishky.proxy.port.flashcard.FlashcardSet;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface FlashcardSetMongoRepository extends
     ReactiveMongoRepository<FlashcardSet, String> {
 
   Mono<FlashcardSet> findByName(String name);
+
+  Flux<FlashcardSet> findAllByOwner(String owner);
 }
