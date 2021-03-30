f93e3b19339897d6ce860966a5600a6e24a13160
Damian Szwed <damian.szwed@10g.pl>
2021-03-30 08:05:22 +0200

    0.9.7 - added one log.

--- src/main/java/com/github/damianszwed/fishky/proxy/business/HealthCheckHandler.java
+++ src/main/java/com/github/damianszwed/fishky/proxy/business/HealthCheckHandler.java
@@ -1,14 +1,17 @@
 package com.github.damianszwed.fishky.proxy.business;
 
 import com.github.damianszwed.fishky.proxy.port.CommandQueryHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public class HealthCheckHandler implements CommandQueryHandler {
 
   @Override
   public Mono<ServerResponse> handle(ServerRequest serverRequest) {
+    log.info("Healtcheck request has been made.");
     return ServerResponse.ok().build();
   }
 }
