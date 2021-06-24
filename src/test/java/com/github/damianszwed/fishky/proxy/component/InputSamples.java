95eba3e53f84704c4a763784eccf0ebff6a43e70
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2021-06-24 08:47:47 +0200

    answer -> answers

--- src/test/java/com/github/damianszwed/fishky/proxy/component/InputSamples.java
+++ src/test/java/com/github/damianszwed/fishky/proxy/component/InputSamples.java
@@ -4,14 +4,14 @@ interface InputSamples {
 
   String NEW_FLASHCARD = "{\n"
       + "  \"question\": \"questionD\",\n"
-      + "  \"answer\": \"answerD\"\n"
+      + "  \"answers\": [\"answerD\"]\n"
       + "}";
   String MODIFIED_FLASHCARD = "{\n"
       + "  \"id\": \"dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25i\",\n"
       + "  \"question\": \"questionDD\",\n"
-      + "  \"answer\": \"answerD"
-      + "D\"\n"
-      + "}";
+      + "  \"answers\": [\"answerDD\"]\n"
+      +
+      "}";
   String EXISTING_FLASHCARD_ID = "dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25i";
   String FLASHCARD_FOLDER_ID = "dXNlcjFAZXhhbXBsZS5jb20tZGVmYXVsdA==";
 }
