b0fed1567ab15e66292beeef5f0be23e8e3147fb
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2021-09-21 16:19:41 +0200

    FlashcardFolderDevelopmentStorage - added brough in folder.

--- src/main/java/com/github/damianszwed/fishky/flashcard/service/adapter/storage/development/FlashcardFolderDevelopmentStorage.java
+++ src/main/java/com/github/damianszwed/fishky/flashcard/service/adapter/storage/development/FlashcardFolderDevelopmentStorage.java
@@ -6,7 +6,6 @@ import static java.util.Collections.singletonList;
 import com.github.damianszwed.fishky.flashcard.service.port.flashcard.Flashcard;
 import com.github.damianszwed.fishky.flashcard.service.port.flashcard.FlashcardFolder;
 import com.github.damianszwed.fishky.flashcard.service.port.flashcard.FlashcardFolderService;
-
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -17,32 +16,48 @@ import reactor.core.publisher.Mono;
 public class FlashcardFolderDevelopmentStorage implements FlashcardFolderService {
 
   private final List<FlashcardFolder> flashcardFolders = new ArrayList<>();
+  @SuppressWarnings("FieldCanBeLocal")
+  private final FlashcardFolder user1Folder = FlashcardFolder.builder()
+      .id("dXNlcjFAZXhhbXBsZS5jb20tZGVmYXVsdA==")//user1@example.com-default
+      .owner("user1@example.com")
+      .name("Default")
+      .flashcards(asList(
+          Flashcard.builder()
+              .id("dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25h")//user1@example.com-questiona
+              .question("questionA")
+              .answers(singletonList("answerA")).build(),
+          Flashcard.builder()
+              .id("dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25i")//user1@example.com-questionb
+              .question("questionB")
+              .answers(singletonList("answerB")).build(),
+          Flashcard.builder()
+              .id("dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25j")//user1@example.com-questionc
+              .question("questionC")
+              .answers(singletonList("answerC")).build()
+      ))
+      .build();
+  @SuppressWarnings("FieldCanBeLocal")
+  private final FlashcardFolder broughtInFolder = FlashcardFolder.builder()
+      .id("YnJvdWdodGluLXR1cmlzbQ==")//broughtin-turism
+      .owner("broughtin")
+      .name("Turism")
+      .flashcards(singletonList(
+          Flashcard.builder()
+              .id("YnJvdWdodGluLWV4Y3Vyc2lvbg==")//broughtin-excursion
+              .question("excursion")
+              .answers(asList("wycieczka", "wyprawa", "wypad")).build()
+      ))
+      .build();
 
   public FlashcardFolderDevelopmentStorage() {
-    flashcardFolders.add(FlashcardFolder.builder()
-        .id("dXNlcjFAZXhhbXBsZS5jb20tZGVmYXVsdA==")//user1@example.com-default
-        .owner("user1@example.com")
-        .name("Default")
-        .flashcards(asList(
-            Flashcard.builder()
-                .id("dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25h")//user1@example.com-questiona
-                .question("questionA")
-                .answers(singletonList("answerA")).build(),
-            Flashcard.builder()
-                .id("dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25i")//user1@example.com-questionb
-                .question("questionB")
-                .answers(singletonList("answerB")).build(),
-            Flashcard.builder()
-                .id("dXNlcjFAZXhhbXBsZS5jb20tcXVlc3Rpb25j")//user1@example.com-questionc
-                .question("questionC")
-                .answers(singletonList("answerC")).build()
-        ))
-        .build());
+    flashcardFolders.add(user1Folder);
+    flashcardFolders.add(broughtInFolder);
   }
 
   @Override
   public Flux<FlashcardFolder> get(String owner) {
-    return Flux.fromIterable(flashcardFolders);
+    return Flux.fromIterable(flashcardFolders)
+        .filter(flashcardFolder -> owner.equals(flashcardFolder.getOwner()));
   }
 
   @Override
