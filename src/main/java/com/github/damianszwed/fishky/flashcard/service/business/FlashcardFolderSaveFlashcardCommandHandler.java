2dede5dc264284f38ba5ba07a3476e62c590eb47
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2021-10-22 09:51:07 +0200

    Polished FlashcardFolderSaveFlashcardCommandHandler

--- src/main/java/com/github/damianszwed/fishky/flashcard/service/business/FlashcardFolderSaveFlashcardCommandHandler.java
+++ src/main/java/com/github/damianszwed/fishky/flashcard/service/business/FlashcardFolderSaveFlashcardCommandHandler.java
@@ -7,6 +7,7 @@ import com.github.damianszwed.fishky.flashcard.service.port.CommandQueryHandler;
 import com.github.damianszwed.fishky.flashcard.service.port.IdEncoderDecoder;
 import com.github.damianszwed.fishky.flashcard.service.port.OwnerProvider;
 import com.github.damianszwed.fishky.flashcard.service.port.flashcard.Flashcard;
+import com.github.damianszwed.fishky.flashcard.service.port.flashcard.FlashcardFolder;
 import com.github.damianszwed.fishky.flashcard.service.port.flashcard.FlashcardFolderService;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 @Slf4j
 public class FlashcardFolderSaveFlashcardCommandHandler implements CommandQueryHandler {
@@ -35,32 +38,42 @@ public class FlashcardFolderSaveFlashcardCommandHandler implements CommandQueryH
   public Mono<ServerResponse> handle(ServerRequest serverRequest) {
     return ownerProvider.provide(serverRequest)
         .map(ownerId -> serverRequest.bodyToMono(Flashcard.class)
-            .doOnNext(flashcardToSave ->
-                flashcardFolderService
-                    .getById(ownerId,
-                        serverRequest.pathVariable("id"))
-                    .subscribe(flashcardFolder -> {
-                      String id = idEncoderDecoder
-                          .encodeId(ownerId,
-                              flashcardToSave.getQuestion());
-                      List<Flashcard> flashcardsWithOneRemoved = flashcardFolder.getFlashcards()
-                          .stream()
-                          .filter(flashcard -> !flashcard.getId().equals(id))
-                          .collect(Collectors.toList());
-                      flashcardsWithOneRemoved.add(flashcardToSave
-                          .toBuilder()
-                          .id(id)
-                          .build());
-                      flashcardFolderService
-                          .save(
-                              ownerId, flashcardFolder.toBuilder()
-                                  .flashcards(flashcardsWithOneRemoved)
-                                  .build())
-                          .subscribe(newFlashcardFolder ->
-                              log.info("FlashcardFolder {} has been saved.",
-                                  newFlashcardFolder.getId()));
-                    }))
+            .map(flashcardToSave -> withFlashcardFolder(serverRequest, ownerId, flashcardToSave))
+            .flatMap(tuples -> saveFolderWithNewFlashcard(ownerId, tuples))
+            .doOnNext(flashcardFolder -> log.info("FlashcardFolder {} has been saved.",
+                flashcardFolder.getId()))
             .flatMap(flashcard -> accepted().build()))
         .orElse(badRequest().build());
+  }
+
+  private Tuple2<Flashcard, Mono<FlashcardFolder>> withFlashcardFolder(ServerRequest serverRequest,
+      String ownerId, Flashcard flashcardToSave) {
+    return Tuples.of(flashcardToSave, flashcardFolderService
+        .getById(ownerId,
+            serverRequest.pathVariable("id")));
+  }
+
+  private Mono<FlashcardFolder> saveFolderWithNewFlashcard(String ownerId,
+      Tuple2<Flashcard, Mono<FlashcardFolder>> tuples) {
+    return tuples.getT2()
+        .flatMap(flashcardFolder -> {
+          Flashcard flashcardToSave = tuples.getT1();
+          String id = idEncoderDecoder
+              .encodeId(ownerId,
+                  flashcardToSave.getQuestion());
+          List<Flashcard> flashcardsWithOneRemoved = flashcardFolder.getFlashcards()
+              .stream()
+              .filter(flashcard -> !flashcard.getId().equals(id))
+              .collect(Collectors.toList());
+          flashcardsWithOneRemoved.add(flashcardToSave
+              .toBuilder()
+              .id(id)
+              .build());
+          return flashcardFolderService
+              .save(
+                  ownerId, flashcardFolder.toBuilder()
+                      .flashcards(flashcardsWithOneRemoved)
+                      .build());
+        });
   }
 }
