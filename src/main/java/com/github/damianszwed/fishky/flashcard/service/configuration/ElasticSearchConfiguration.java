4835af233317cdd47917db5941cf995f5e27bbdf
Damian Szwed <damian.szwed@10g.pl>
2023-01-03 13:21:55 +0100

    Elasticsearch - search without check

--- src/main/java/com/github/damianszwed/fishky/flashcard/service/configuration/ElasticSearchConfiguration.java
+++ src/main/java/com/github/damianszwed/fishky/flashcard/service/configuration/ElasticSearchConfiguration.java
@@ -1,7 +1,14 @@
 package com.github.damianszwed.fishky.flashcard.service.configuration;
 
-import com.github.damianszwed.fishky.flashcard.service.adapter.search.ElasticSearchFlashcardSearchService;
+import com.github.damianszwed.fishky.flashcard.service.adapter.search.elastic.ElasticSearchFlashcardSearchService;
+import com.github.damianszwed.fishky.flashcard.service.adapter.search.elastic.ElasticSearchRename;
+import com.github.damianszwed.fishky.flashcard.service.port.flashcard.FlashcardFolderService;
 import com.github.damianszwed.fishky.flashcard.service.port.flashcard.FlashcardSearchService;
+import java.net.URI;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,7 +18,22 @@ import org.springframework.context.annotation.Profile;
 public class ElasticSearchConfiguration {
 
   @Bean
-  FlashcardSearchService flashcardSearchService() {
-    return new ElasticSearchFlashcardSearchService();
+  ElasticSearchRename elasticSearchRename(
+      @Value("${fishky.elasticsearch.uri}") String elasticSearchUri,
+      @Value("${fishky.elasticsearch.index:flashcards-000001") String index) {
+    final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    final URI connUri = URI.create(elasticSearchUri);
+    final String[] auth = connUri.getUserInfo().split(":");
+    credentialsProvider.setCredentials(AuthScope.ANY,
+        new UsernamePasswordCredentials(auth[0], auth[1]));
+
+    return new ElasticSearchRename(index, credentialsProvider, connUri);
+  }
+
+  @Bean
+  FlashcardSearchService flashcardSearchService(
+      FlashcardFolderService flashcardFolderStorage,
+      ElasticSearchRename elasticSearchRename) {
+    return new ElasticSearchFlashcardSearchService(flashcardFolderStorage, elasticSearchRename);
   }
 }
