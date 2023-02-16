f6f17d8d76f31629dad5a70592c6f7030ec3d742
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2023-02-16 15:34:14 +0100

    Mongodb without security profile - dev purpose.

--- src/main/java/com/github/damianszwed/fishky/flashcard/service/configuration/ProductionSpecificConfiguration.java
+++ src/main/java/com/github/damianszwed/fishky/flashcard/service/configuration/ProductionSpecificConfiguration.java
@@ -1,18 +1,14 @@
 package com.github.damianszwed.fishky.flashcard.service.configuration;
 
-import com.github.damianszwed.fishky.flashcard.service.adapter.security.ProductionOwnerProvider;
 import com.github.damianszwed.fishky.flashcard.service.adapter.storage.production.FlashcardFolderMongoRepository;
 import com.github.damianszwed.fishky.flashcard.service.adapter.storage.production.FlashcardFolderProductionStorage;
-import com.github.damianszwed.fishky.flashcard.service.port.OwnerProvider;
 import com.github.damianszwed.fishky.flashcard.service.port.flashcard.FlashcardFolderService;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
 
 @Configuration
-@Profile("production")
+@Profile({"production", "mongo-development"})//TODO(Damian.Szwed)After search dev remove mongo-development entry
 public class ProductionSpecificConfiguration {
 
   @Bean
@@ -21,9 +17,4 @@ public class ProductionSpecificConfiguration {
     return new FlashcardFolderProductionStorage(flashcardFolderMongoRepository);
   }
 
-  @Bean
-  OwnerProvider ownerProvider(OAuth2ResourceServerProperties properties) {
-    return new ProductionOwnerProvider(
-        JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri()));
-  }
 }
