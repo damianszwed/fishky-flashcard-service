6ce1db68a7ab4d04107eb440b308b5629010e6d7
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2023-03-09 09:32:30 +0100

    Update src/main/java/com/github/damianszwed/fishky/flashcard/service/configuration/DevelopmentSpecificConfiguration.java

--- src/main/java/com/github/damianszwed/fishky/flashcard/service/configuration/DevelopmentSpecificConfiguration.java
+++ src/main/java/com/github/damianszwed/fishky/flashcard/service/configuration/DevelopmentSpecificConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"development"})
+@Profile("development")
 public class DevelopmentSpecificConfiguration {
 
   @Bean
