187f5434e71d37e458f2006de616ccbcf767c26d
Damian Szwed <34833355+damianszwed@users.noreply.github.com>
2023-12-01 21:04:02 +0100

    isOwner hint

--- src/main/java/com/github/damianszwed/fishky/flashcard/service/port/flashcard/ShareMode.java
+++ src/main/java/com/github/damianszwed/fishky/flashcard/service/port/flashcard/ShareMode.java
@@ -1,2 +1,10 @@
-package com.github.damianszwed.fishky.flashcard.service.port.flashcard;public enum ShareMode {
+package com.github.damianszwed.fishky.flashcard.service.port.flashcard;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public enum ShareMode {
+  @JsonProperty("Editor")
+  EDITOR,
+  @JsonProperty("View")
+  VIEW
 }
